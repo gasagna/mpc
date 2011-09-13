2d0075bbeae8854eb3b9910ac8ecd622e1843e47
Davide Lasagna <lasagnadavide@gmail.com>
2011-09-13 23:01:05 +0200

    Modified licence and description in setup.py and modified README.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup, Extension
 
 
-setup(  name        = "pympc",
+setup(  name        = "mpc",
         author      =  "Davide Lasagna",
         author_email = "davide.lasagna@polito.it",
-        license     = "GPL",
+        license     = "GPLv3",
         url         = 'None',
         version     = "0.0.1",
-        description = "A module for simulation of discrete linear time invariant systems controlled with Model Predictive Controllers",
+        description = "A module for simulation of discrete linear time invariant systems controlled with a model predictive controller.",
         packages = ['mpc'], 
         )
